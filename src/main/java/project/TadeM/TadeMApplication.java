85f428e76325c3f9a386ec50f323b213afa0a62e
shinyurim <yurim12@naver.com>
2022-10-20 21:51:07 +0900

    Init

---
+++ src/main/java/project/TadeM/TadeMApplication.java
@@ -0,0 +1,13 @@
+package project.TadeM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadeMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadeMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/TadeM/TadeMApplicationTests.java
@@ -0,0 +1,13 @@
+package project.TadeM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadeMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
