5b0bcf260cf2f157b886d988f7e74067ad2cc3af
shinyurim <yurim12@naver.com>
2022-10-22 21:14:54 +0900

    init

---
+++ src/test/java/project/TadeM/SampleTest.java
@@ -0,0 +1,16 @@
+package project.TadeM;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+	@Test
+	void sampleTest(){
+		assertEquals(1,1);
+	}
+
+}
